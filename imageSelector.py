6b214c43e4947728415bf44ebf0e8a9f10a0dd99
Developer Pancake <adamwarren102@gmail.com>
2026-01-27 06:55:52 -0600

    Added tabs to have the images

--- imageSelector.py
+++ imageSelector.py
@@ -1,13 +1,27 @@
 import tkinter as tk
+from tkinter import ttk
 from tkinter import filedialog, Tk, Label
 from PIL import Image, ImageTk # type: ignore
 
 root = Tk()
+root.geometry("500x500")
+root.title("Image Loader")
+
+notebook = ttk.Notebook(root)
+notebook.pack(fill='both', expand=True)
+
+tab1 = ttk.Frame(notebook)
+tab2 = ttk.Frame(notebook)
+tab3 = ttk.Frame(notebook)
+
+notebook.add(tab1, text="Tab 1")
+notebook.add(tab2, text="Tab 2")
+notebook.add(tab3, text="Tab 3")
 
 label = Label(root)
 label.pack()
 
-def load_image():
+def load_image_tabone():
     file_path = filedialog.askopenfilename(
         filetypes=[
             ("Images", "*.png *.jpg *.jpeg")
@@ -16,11 +30,44 @@ def load_image():
     if not file_path:
         return
     img = Image.open(file_path)
+    img = img.resize((400, 400))
     photo = ImageTk.PhotoImage(img)
-    label.config(image=photo)
-    label.image = photo
+    image_label = Label(tab1, image=photo)
+    image_label.image = photo
+    image_label.pack()
 
-button = tk.Button(text="Load image", command=load_image)
-button.pack()
+def load_image_tabtwo():
+    file_path = filedialog.askopenfilename(
+        filetypes=[
+            ("Images", "*.png *.jpg *.jpeg")
+        ]
+    )
+    if not file_path:
+        return
+    img = Image.open(file_path)
+    img = img.resize((400, 400))
+    photo = ImageTk.PhotoImage(img)
+    image_label = Label(tab2, image=photo)
+    image_label.image = photo
+    image_label.pack()
+
+def load_image_tabthree():
+    file_path = filedialog.askopenfilename(
+        filetypes=[
+            ("Images", "*.png *.jpg *.jpeg")
+        ]
+    )
+    if not file_path:
+        return
+    img = Image.open(file_path)
+    img = img.resize((400, 400))
+    photo = ImageTk.PhotoImage(img)
+    image_label = Label(tab3, image=photo)
+    image_label.image = photo
+    image_label.pack()
+
+button = tk.Button(tab1, text="Load image", command=load_image_tabone).pack()
+button = tk.Button(tab2, text="Load image", command=load_image_tabtwo).pack()
+button = tk.Button(tab3, text="Load image", command=load_image_tabthree).pack()
 
 root.mainloop()
